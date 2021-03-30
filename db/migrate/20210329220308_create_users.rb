00a8673eae3bf3dcc695efc913b50d560f0c638f
Erin Anderson <e.anderson1226@gmail.com>
2021-03-30 14:47:58 -0400

    trouble creating the databased for sinatra project

--- db/migrate/20210329220308_create_users.rb
+++
@@ -1,9 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[6.1]
-  def change
-    create_table :users do |u|
-      u.string :name
-      u.string :email
-      u.string :password
-    end
-  end
-end
--- db/migrate/20210329220329_create_assignments.rb
+++
@@ -1,8 +0,0 @@
-class CreateAssignments < ActiveRecord::Migration[6.1]
-  def change
-    create_table :assignments do |t|
-      t.string :title
-      t.string :content
-    end
-  end
-end
