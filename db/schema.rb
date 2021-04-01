559833f9ab8fd033dc2e372eb2d18017f3356093
Erin Anderson <e.anderson1226@gmail.com>
2021-04-01 19:41:43 -0400

    my schema file being created after migrating my tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_184621) do
+ActiveRecord::Schema.define(version: 2021_04_01_233315) do
 
   create_table "assignments", force: :cascade do |t|
     t.string "title"
