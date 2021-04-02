698b5729a1d28d666eee06d889f85ba8f963d622
Erin Anderson <e.anderson1226@gmail.com>
2021-04-01 21:59:09 -0400

    creating the get '/signup' route and the get '/lougout' routes for UserController class

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,3 +1,19 @@
 class UserController < ApplicationController
 
+    #show sign up form
+    get '/signup' do
+        erb :"user/signup"
+    end
+
+    #post sign up form
+
+    #show login form
+    
+    #post login form 
+
+    #have the user logout
+    get '/logout' do
+        sessions.clear
+    end
+
 end
