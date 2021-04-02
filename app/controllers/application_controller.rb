8fa40e0f58e60c84bfda94ac66660739b4ba84d5
Erin Anderson <e.anderson1226@gmail.com>
2021-04-01 21:57:30 -0400

    Enabling sessions and setting sessions_secret for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,12 +5,13 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :sessions_secret, 'kefnlnsdlkdmdl'
   end
 
   #show first page show all the assignments uploaded
   get "/" do
     erb :welcome
   end
-
 
 end
